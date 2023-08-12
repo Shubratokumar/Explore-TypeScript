1e96cd540bd6c44d63479019a814ba31affd245e
DevShubrato <shubratokumargharami920@gmail.com>
2023-08-12 23:55:58 +0600

    basics about ts

---
+++ basic.js
@@ -0,0 +1,6 @@
+let user = {
+    name: "shubrato",
+    age: 26
+};
+
+console.log(user.email);
---
+++ basics.ts
@@ -0,0 +1,7 @@
+let user = {
+    name: "shubrato",
+    age: 26
+};
+const email = user.email;
+console.log(email, "....");
+// console.log(user.name, user.age);
