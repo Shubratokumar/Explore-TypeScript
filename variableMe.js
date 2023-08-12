8b8ea2d106547cd0b3c12e35b4c84a531f93adc8
DevShubrato <shubratokumargharami920@gmail.com>
2023-08-13 01:14:51 +0600

    String type

---
+++ variableMe.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greetings = "Hello Shubrato";
+// greetings = true;
+var myNum = 38;
+// myNum.toLowerCase();
+console.log(greetings.toLowerCase());
+console.log(greetings);
---
+++ variableMe.ts
@@ -0,0 +1,8 @@
+let greetings: string  = "Hello Shubrato";
+
+// greetings = true;
+let myNum = 38;
+// myNum.toLowerCase();
+console.log(greetings.toLowerCase());
+console.log(greetings);
+export {}
