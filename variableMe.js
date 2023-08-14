9521b5607391013de06399a676f424f097ffe70a
DevShubrato <shubratokumargharami920@gmail.com>
2023-08-14 17:20:13 +0600

    any type in ts

--- variableMe.js
+++ variableMe.js
@@ -2,10 +2,18 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 // String 
 var greetings = "Hello Shubrato";
-console.log(greetings.toLowerCase());
+// console.log(greetings.toLowerCase());
 // number
 var userId = 446699.5;
-console.log(userId);
+// console.log(userId);
 // boolean
 var isLoggedIn = false;
-console.log(isLoggedIn);
+isLoggedIn.valueOf();
+// console.log(isLoggedIn);
+// any 
+var hero;
+function getHero() {
+    return "Thor";
+}
+hero = getHero();
+console.log(hero);
