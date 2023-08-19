96951f7a04cadf0a67b857c23e4e30d29044c24d
DevShubrato <shubratokumargharami920@gmail.com>
2023-08-19 23:52:43 +0600

    more about the function in ts

--- myFunction.ts
+++ myFunction.ts
@@ -1,5 +1,6 @@
-function addTwo(num: number){
+function addTwo(num: number): number{
     // num.toUpperCase(); not accepted as type number
+    // return "Hello";
     return num + 5;
 }
 addTwo(7);// we can't pass a string here as an argument because type number.
@@ -16,5 +17,38 @@ signUpUser("shuvo", "shuvo@gmail.com", true );
 let loginUser = (name: string, email: string, isPaid: boolean = false) => {}; // arrow function with default value
 
 loginUser("sk", "sk@gmail.com");
+
+//// more about function
+// explicitly mentioned the type 
+/* function getValue (myVal: number): string | boolean {
+    if(myVal > 5){
+        return true;
+    }
+    return "200 Ok";
+}
+getValue(10); */
+
+// basic of arrow functions: return any value even an empty string
+const getHello = (s: string): string =>{
+    return "";
+}
+
+const heros = ["thor", "spiderman", "ironman", "batman"];
+// const heros = [2, 3, 5, 6, 7];
+heros.map((hero): string =>{
+    return `my hero is ${hero}.`;
+    // return 5;
+});
+
+// when we don't want to return anything from a function then explicitly set the type to void. After setting the type to void we can't return anything from the function. This will give us an error.
+function consoleError(errmsg: string): void {
+    console.error(errmsg);
+    // return 5;
+}
+// using your function robustly use never type instead of void.
+function handleError(errmsg: string): never {
+    throw new Error(errmsg);
+}
+
 
 export {}
