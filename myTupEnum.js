d1daff6279ac163d4b7aa64da3a9b0e7d22c7ad6
DevShubrato <shubratokumargharami920@gmail.com>
2023-08-25 11:08:27 +0600

    tuples in ts

---
+++ myTupEnum.js
@@ -0,0 +1,7 @@
+"use strict";
+// In board category, it's just an array. Kind of arrays that given by typescript with some restrictions on to it.
+Object.defineProperty(exports, "__esModule", { value: true });
+// Examples
+// completely okay. no need to maintain the sequence that is 1st number and 2nd string like that.
+var user = [1, "sk"];
+var user2 = ["sk", 1];
