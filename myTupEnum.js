1620f8b14e2b2d8369f13518bceedceb77213433
DevShubrato <shubratokumargharami920@gmail.com>
2023-08-31 18:50:40 +0600

    enum in ts

--- myTupEnum.js
+++ myTupEnum.js
@@ -5,3 +5,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // completely okay. no need to maintain the sequence that is 1st number and 2nd string like that.
 var user = [1, "sk"];
 var user2 = ["sk", 1];
+// tuple example
+// it's all about strict sequent of data types in an array.
+// knows exactly how many elements it contains, and exactly which types it contains at specific positions.
+var user3;
+user3 = ["sk", 3, true]; // must maintain the order of the data types in the array.
+var rgb = [255, 123, 243];
+var newUser = [122, "sk@gmail.com"];
+newUser[1] = "sk.com";
