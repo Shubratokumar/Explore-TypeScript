71d12b6d219af1394897a1c3fc967a4a0f245b59
DevShubrato <shubratokumargharami920@gmail.com>
2023-08-31 19:09:39 +0600

    interfaces in ts

---
+++ myInterface.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var shuvo = {
+    dbId: 55,
+    email: "sk@gmail.com",
+    userId: 4444,
+    startTrail: function () {
+        return "Trial Started!";
+    },
+    getCoupon: function (name) {
+        return 77;
+    },
+};
+shuvo.userId = 7777;
+shuvo.email = "skg@gmail.com";
---
+++ myInterface.ts
@@ -0,0 +1,31 @@
+interface User {
+    readonly dbId: number;
+    email: string,
+    userId: number,
+    googleId?: string,// optional
+    //startTrail: () => string // method that returns string
+    startTrail(): string,
+    getCoupon(couponname: string): number
+}
+
+const shuvo: User = {
+    dbId: 55,
+    email: "sk@gmail.com",
+    userId: 4444,
+    startTrail:  () => {
+        return "Trial Started!";
+    }, 
+    getCoupon: (name: "shuvo100") => {
+        return 77
+    },
+};
+shuvo.userId = 7777;
+shuvo.email = "skg@gmail.com";
+//shuvo.dbId = 55; //Cannot do so because 'dbId' is a read-only property
+
+
+
+
+
+
+export {}
