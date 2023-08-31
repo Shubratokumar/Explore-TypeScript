ec788c45b8d6458e641ed609ef18c64ca7312c1d
DevShubrato <shubratokumargharami920@gmail.com>
2023-08-31 18:52:53 +0600

    effi remove with const

--- myEnums.js
+++ myEnums.js
@@ -1,11 +1,3 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// define enum
-var SeatChoice;
-(function (SeatChoice) {
-    SeatChoice["AISLE"] = "alise";
-    SeatChoice[SeatChoice["MIDDLE"] = 4] = "MIDDLE";
-    SeatChoice[SeatChoice["WINDOW"] = 5] = "WINDOW";
-    SeatChoice[SeatChoice["FOURTH"] = 6] = "FOURTH";
-})(SeatChoice || (SeatChoice = {}));
-var skSeat = SeatChoice.AISLE;
+var skSeat = "alise" /* SeatChoice.AISLE */;
--- myEnums.ts
+++ myEnums.ts
@@ -1,5 +1,5 @@
 // define enum
-enum SeatChoice {
+const enum SeatChoice {
     AISLE = "alise", // by default it takes number value 1. we can assign any number or string value. if we set the number then rest will take subsequent numeric values.
     MIDDLE = 4, // once we set string rest must be defined. if number rest will take automatically.
     WINDOW,
